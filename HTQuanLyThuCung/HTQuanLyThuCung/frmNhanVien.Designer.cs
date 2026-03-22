49de6f7d7e139e6cd47be5f47117023601b6238d
LÊ NHI <leyennhi170705@gmail.com>
2026-03-22 16:59:22 +0700

    frmNhanVien

---
+++ HTQuanLyThuCung/HTQuanLyThuCung/frmNhanVien.Designer.cs
@@ -0,0 +1,252 @@
+﻿namespace HTQuanLyThuCung
+{
+    partial class frmNhanVien
+    {
+        private System.ComponentModel.IContainer components = null;
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.txtTen = new System.Windows.Forms.TextBox();
+            this.txtSdt = new System.Windows.Forms.TextBox();
+            this.txtDiaChi = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtCccd = new System.Windows.Forms.TextBox();
+            this.cboCaLam = new System.Windows.Forms.ComboBox();
+            this.btnThem = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.dgvNhanVien = new System.Windows.Forms.DataGridView();
+            this.lblTen = new System.Windows.Forms.Label();
+            this.lblSdt = new System.Windows.Forms.Label();
+            this.lblDiaChi = new System.Windows.Forms.Label();
+            this.lblEmail = new System.Windows.Forms.Label();
+            this.lblCccd = new System.Windows.Forms.Label();
+            this.lblCaLam = new System.Windows.Forms.Label();
+            this.pnlTop = new System.Windows.Forms.Panel();
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNhanVien)).BeginInit();
+            this.pnlTop.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // txtTen
+            // 
+            this.txtTen.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtTen.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.txtTen.Location = new System.Drawing.Point(20, 46);
+            this.txtTen.Name = "txtTen";
+            this.txtTen.Size = new System.Drawing.Size(430, 43);
+            this.txtTen.TabIndex = 1;
+            // 
+            // txtSdt
+            // 
+            this.txtSdt.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtSdt.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.txtSdt.Location = new System.Drawing.Point(480, 46);
+            this.txtSdt.Name = "txtSdt";
+            this.txtSdt.Size = new System.Drawing.Size(430, 43);
+            this.txtSdt.TabIndex = 3;
+            // 
+            // txtDiaChi
+            // 
+            this.txtDiaChi.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtDiaChi.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.txtDiaChi.Location = new System.Drawing.Point(20, 142);
+            this.txtDiaChi.Name = "txtDiaChi";
+            this.txtDiaChi.Size = new System.Drawing.Size(430, 43);
+            this.txtDiaChi.TabIndex = 5;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtEmail.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.txtEmail.Location = new System.Drawing.Point(480, 142);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(430, 43);
+            this.txtEmail.TabIndex = 7;
+            // 
+            // txtCccd
+            // 
+            this.txtCccd.BackColor = System.Drawing.Color.Gainsboro;
+            this.txtCccd.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.txtCccd.Location = new System.Drawing.Point(20, 232);
+            this.txtCccd.Name = "txtCccd";
+            this.txtCccd.Size = new System.Drawing.Size(430, 43);
+            this.txtCccd.TabIndex = 9;
+            // 
+            // cboCaLam
+            // 
+            this.cboCaLam.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cboCaLam.Font = new System.Drawing.Font("Segoe UI", 16F);
+            this.cboCaLam.Items.AddRange(new object[] {
+            "Sáng",
+            "Chiều",
+            "Tối"});
+            this.cboCaLam.Location = new System.Drawing.Point(480, 232);
+            this.cboCaLam.Name = "cboCaLam";
+            this.cboCaLam.Size = new System.Drawing.Size(430, 45);
+            this.cboCaLam.TabIndex = 11;
+            // 
+            // btnThem
+            // 
+            this.btnThem.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(46)))), ((int)(((byte)(204)))), ((int)(((byte)(113)))));
+            this.btnThem.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnThem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnThem.Location = new System.Drawing.Point(20, 306);
+            this.btnThem.Name = "btnThem";
+            this.btnThem.Size = new System.Drawing.Size(139, 50);
+            this.btnThem.TabIndex = 12;
+            this.btnThem.Text = "Thêm";
+            this.btnThem.UseVisualStyleBackColor = false;
+            this.btnThem.Click += new System.EventHandler(this.btnThem_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(156)))), ((int)(((byte)(18)))));
+            this.btnSua.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnSua.Location = new System.Drawing.Point(189, 306);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(139, 50);
+            this.btnSua.TabIndex = 13;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = false;
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(76)))), ((int)(((byte)(60)))));
+            this.btnXoa.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.btnXoa.ForeColor = System.Drawing.Color.White;
+            this.btnXoa.Location = new System.Drawing.Point(347, 306);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(139, 50);
+            this.btnXoa.TabIndex = 14;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = false;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // dgvNhanVien
+            // 
+            this.dgvNhanVien.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dgvNhanVien.BackgroundColor = System.Drawing.Color.White;
+            this.dgvNhanVien.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.dgvNhanVien.ColumnHeadersHeight = 29;
+            this.dgvNhanVien.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.dgvNhanVien.Font = new System.Drawing.Font("Segoe UI", 11F);
+            this.dgvNhanVien.Location = new System.Drawing.Point(0, 420);
+            this.dgvNhanVien.Name = "dgvNhanVien";
+            this.dgvNhanVien.RowHeadersVisible = false;
+            this.dgvNhanVien.RowHeadersWidth = 51;
+            this.dgvNhanVien.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.dgvNhanVien.Size = new System.Drawing.Size(950, 430);
+            this.dgvNhanVien.TabIndex = 0;
+            // 
+            // lblTen
+            // 
+            this.lblTen.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblTen.Location = new System.Drawing.Point(20, 20);
+            this.lblTen.Name = "lblTen";
+            this.lblTen.Size = new System.Drawing.Size(100, 23);
+            this.lblTen.TabIndex = 0;
+            this.lblTen.Text = "Tên nhân viên";
+            // 
+            // lblSdt
+            // 
+            this.lblSdt.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblSdt.Location = new System.Drawing.Point(480, 20);
+            this.lblSdt.Name = "lblSdt";
+            this.lblSdt.Size = new System.Drawing.Size(100, 23);
+            this.lblSdt.TabIndex = 2;
+            this.lblSdt.Text = "Số điện thoại";
+            // 
+            // lblDiaChi
+            // 
+            this.lblDiaChi.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblDiaChi.Location = new System.Drawing.Point(20, 106);
+            this.lblDiaChi.Name = "lblDiaChi";
+            this.lblDiaChi.Size = new System.Drawing.Size(100, 23);
+            this.lblDiaChi.TabIndex = 4;
+            this.lblDiaChi.Text = "Địa chỉ";
+            // 
+            // lblEmail
+            // 
+            this.lblEmail.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblEmail.Location = new System.Drawing.Point(480, 106);
+            this.lblEmail.Name = "lblEmail";
+            this.lblEmail.Size = new System.Drawing.Size(100, 23);
+            this.lblEmail.TabIndex = 6;
+            this.lblEmail.Text = "Email";
+            // 
+            // lblCccd
+            // 
+            this.lblCccd.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblCccd.Location = new System.Drawing.Point(20, 202);
+            this.lblCccd.Name = "lblCccd";
+            this.lblCccd.Size = new System.Drawing.Size(100, 23);
+            this.lblCccd.TabIndex = 8;
+            this.lblCccd.Text = "Căn cước công dân";
+            // 
+            // lblCaLam
+            // 
+            this.lblCaLam.Font = new System.Drawing.Font("Segoe UI", 14F, System.Drawing.FontStyle.Bold);
+            this.lblCaLam.Location = new System.Drawing.Point(480, 202);
+            this.lblCaLam.Name = "lblCaLam";
+            this.lblCaLam.Size = new System.Drawing.Size(100, 23);
+            this.lblCaLam.TabIndex = 10;
+            this.lblCaLam.Text = "Ca làm việc";
+            // 
+            // pnlTop
+            // 
+            this.pnlTop.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(240)))), ((int)(((byte)(240)))));
+            this.pnlTop.Controls.Add(this.lblTen);
+            this.pnlTop.Controls.Add(this.txtTen);
+            this.pnlTop.Controls.Add(this.lblSdt);
+            this.pnlTop.Controls.Add(this.txtSdt);
+            this.pnlTop.Controls.Add(this.lblDiaChi);
+            this.pnlTop.Controls.Add(this.txtDiaChi);
+            this.pnlTop.Controls.Add(this.lblEmail);
+            this.pnlTop.Controls.Add(this.txtEmail);
+            this.pnlTop.Controls.Add(this.lblCccd);
+            this.pnlTop.Controls.Add(this.txtCccd);
+            this.pnlTop.Controls.Add(this.lblCaLam);
+            this.pnlTop.Controls.Add(this.cboCaLam);
+            this.pnlTop.Controls.Add(this.btnThem);
+            this.pnlTop.Controls.Add(this.btnSua);
+            this.pnlTop.Controls.Add(this.btnXoa);
+            this.pnlTop.Dock = System.Windows.Forms.DockStyle.Top;
+            this.pnlTop.Location = new System.Drawing.Point(0, 0);
+            this.pnlTop.Name = "pnlTop";
+            this.pnlTop.Size = new System.Drawing.Size(950, 420);
+            this.pnlTop.TabIndex = 1;
+            // 
+            // frmNhanVien
+            // 
+            this.ClientSize = new System.Drawing.Size(950, 850);
+            this.Controls.Add(this.dgvNhanVien);
+            this.Controls.Add(this.pnlTop);
+            this.Name = "frmNhanVien";
+            this.Text = "Quản lý nhân viên";
+            this.Load += new System.EventHandler(this.frmNhanVien_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dgvNhanVien)).EndInit();
+            this.pnlTop.ResumeLayout(false);
+            this.pnlTop.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        private System.Windows.Forms.TextBox txtTen, txtSdt, txtDiaChi, txtEmail, txtCccd;
+        private System.Windows.Forms.ComboBox cboCaLam;
+        private System.Windows.Forms.Button btnThem, btnSua, btnXoa;
+        private System.Windows.Forms.DataGridView dgvNhanVien;
+        private System.Windows.Forms.Label lblTen, lblSdt, lblDiaChi, lblEmail, lblCccd, lblCaLam;
+        private System.Windows.Forms.Panel pnlTop;
+    }
+}
---
+++ HTQuanLyThuCung/HTQuanLyThuCung/frmNhanVien.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace HTQuanLyThuCung
+{
+    public partial class frmNhanVien : Form
+    {
+        public frmNhanVien()
+        {
+            InitializeComponent();
+        }
+
+        private void frmNhanVien_Load(object sender, EventArgs e)
+        {
+            // Thiết lập các cột hiển thị
+            dgvNhanVien.Columns.Clear();
+            dgvNhanVien.Columns.Add("id", "#");
+            dgvNhanVien.Columns.Add("ten", "Tên nhân viên");
+            dgvNhanVien.Columns.Add("sdt", "Số điện thoại");
+            dgvNhanVien.Columns.Add("dc", "Địa chỉ");
+            dgvNhanVien.Columns.Add("em", "Email");
+            dgvNhanVien.Columns.Add("cc", "CCCD");
+            dgvNhanVien.Columns.Add("ca", "Ca làm");
+
+            dgvNhanVien.Columns["id"].FillWeight = 30; // Cột số thứ tự nhỏ lại cho cân đối
+
+            // Mới chạy ô Ca làm việc sẽ trống
+            cboCaLam.SelectedIndex = -1;
+
+            LoadData();
+        }
+
+        void LoadData()
+        {
+            dgvNhanVien.Rows.Clear();
+            // Nạp dữ liệu 15 nhân viên TP.HCM
+            dgvNhanVien.Rows.Add("1", "Nguyễn Văn An", "0901234567", "Quận 1, TP HCM", "an.nguyen@gmail.com", "079090001234", "Ca Sáng");
+            dgvNhanVien.Rows.Add("2", "Lê Thị Bình", "0912345678", "Quận 3, TP HCM", "binh.le@gmail.com", "079091005678", "Ca Chiều");
+            dgvNhanVien.Rows.Add("3", "Trần Minh Cường", "0923456789", "Quận Bình Thạnh, TP HCM", "cuong.tran@gmail.com", "079092009012", "Ca Tối");
+            dgvNhanVien.Rows.Add("4", "Phạm Thanh Dung", "0934567890", "Quận Tân Bình, TP HCM", "dung.pham@gmail.com", "079093003456", "Ca Sáng");
+            dgvNhanVien.Rows.Add("5", "Hoàng Anh Tuấn", "0945678901", "Quận Gò Vấp, TP HCM", "tuan.hoang@gmail.com", "079094007890", "Ca Chiều");
+            dgvNhanVien.Rows.Add("6", "Vũ Mỹ Hạnh", "0956789012", "Quận Phú Nhuận, TP HCM", "hanh.vu@gmail.com", "079095001234", "Ca Tối");
+            dgvNhanVien.Rows.Add("7", "Đặng Hữu Phước", "0967890123", "Quận 7, TP HCM", "phuoc.dang@gmail.com", "079096005678", "Ca Sáng");
+            dgvNhanVien.Rows.Add("8", "Bùi Minh Tâm", "0978901234", "Quận 10, TP HCM", "tam.bui@gmail.com", "079097009012", "Ca Chiều");
+            dgvNhanVien.Rows.Add("9", "Đỗ Kim Liên", "0989012345", "Quận 12, TP HCM", "lien.do@gmail.com", "079098003456", "Ca Tối");
+
+            dgvNhanVien.ClearSelection();
+        }
+
+        private void btnThem_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtTen.Text) || cboCaLam.SelectedIndex == -1)
+            {
+                MessageBox.Show("Vui lòng nhập đầy đủ thông tin!");
+                return;
+            }
+            int id = dgvNhanVien.Rows.Count + 1;
+            dgvNhanVien.Rows.Add(id.ToString(), txtTen.Text, txtSdt.Text, txtDiaChi.Text, txtEmail.Text, txtCccd.Text, cboCaLam.Text);
+
+            // Clear và reset combo
+            txtTen.Clear(); txtSdt.Clear(); txtDiaChi.Clear(); txtEmail.Clear(); txtCccd.Clear();
+            cboCaLam.SelectedIndex = -1;
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (dgvNhanVien.SelectedRows.Count > 0)
+            {
+                dgvNhanVien.Rows.RemoveAt(dgvNhanVien.SelectedRows[0].Index);
+            }
+        }
+    }
+}
